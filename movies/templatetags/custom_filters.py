c0d309c828d142a4dec92d4ddb155d7466419909
CurlyRishabh <rissag940@gmail.com>
2024-06-10 11:50:04 +0530

    added rating modal box

--- movies/templatetags/custom_filters.py
+++ movies/templatetags/custom_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='split_by_comma')
 def split_by_comma(value):
     return value.split(', ')
+
+
+@register.filter(name='times') 
+def times(number):
+    return range(number)
